67937686b43ad1e92b895ea90f408898cd795dee
Kimfucious <kimfucious@abts.io>
2020-04-28 17:40:25 +0700

    refactor: separate helpers from validators

---
+++ src/validators/index.js
@@ -0,0 +1,29 @@
+import * as Yup from "yup";
+
+export const confirmationSchema = Yup.object({
+  code: Yup.string()
+    .length(6, "Must be at least exactly 6 characters")
+    .required("Required"),
+  username: Yup.string()
+    .min(2, "Must be at least 2 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Required")
+});
+export const signInSchema = Yup.object({
+  username: Yup.string()
+    .min(2, "Must be at least 2 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Required")
+});
+export const signUpSchema = Yup.object({
+  appleEmail: Yup.string().email("Invalid email address"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  googleEmail: Yup.string().email("Invalid email address"),
+  password: Yup.string()
+    .min(8, "Must be at least 8 characters")
+    .required("Required"),
+  username: Yup.string()
+    .min(2, "Must be at least 2 characters")
+    .max(15, "Must be 15 characters or less")
+    .required("Required")
+});
